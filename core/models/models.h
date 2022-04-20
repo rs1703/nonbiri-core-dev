de7b8a6cec7f2abdc23f43df424e7ddee949f5c9
rs <96896020+rs1703@users.noreply.github.com>
2022-04-20 12:45:03 +0700

    add field path to struct Manga & Chapter

--- core/models/models.h
+++ core/models/models.h
@@ -14,11 +14,19 @@ enum MangaStatus
 
 struct Manga
 {
+  // Full path (e.g., domain.com/manga/path)
   std::string url;
+
+  // Optional, will be taken from url if empty (e.g., /manga/path)
+  std::string path;
+
+  // Full path (e.g., domain.com/covers/path)
   std::string coverUrl;
 
   std::string title;
   std::string description;
+
+  // Publication status
   MangaStatus status;
 
   std::vector<std::string> artists;
@@ -28,7 +36,12 @@ struct Manga
 
 struct Chapter
 {
+  // Full path (e.g., domain.com/chapter/path)
   std::string url;
+
+  // Optional, will be taken from url if empty (e.g., /chapter/path)
+  std::string path;
+
   std::string name;
   int publishedAt;
   std::vector<std::string> groups;
